7c3bcdf7bdcdcf3e1b016ef2b47b9a7e22dd07e9
Ali <avohra@elevatesaas.com>
2021-07-09 13:09:20 +0530

    added submodule

--- SubModule.Consumer/SubModule.Consumer/TestConsumer.cs
+++ SubModule.Consumer/SubModule.Consumer/TestConsumer.cs
@@ -1,9 +1,17 @@
-﻿namespace SubModule.Consumer
+﻿using SubModule.Models;
+
+namespace SubModule.Consumer
 {
     public class TestConsumer
     {
         public TestConsumer()
         {
+            Test test = new Test
+            {
+                Id = 1,
+                Name = "First Test",
+                Address = "Vadodara"
+            };
 
         }
     }
